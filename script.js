9806dac918758564fe8868309912bca91481d678
Abhijith Saju <abhijithsaju@yahoo.com>
2025-07-20 20:39:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function hideLoadingScreen() {
   const loadingScreen = document.getElementById('loading-screen');
   if (loadingScreen) {
-    loadingScreen.classList.add('hidden');
-    
-    setTimeout(() => {
-      loadingScreen.style.display = 'none';
-    }, 500);
+    loadingScreen.style.display = 'none';
   }
 }
 
